8fdcff5ba61c7a73a4507e52cbca10f50c2f3d3f
glenn packer <glenn.packer@yahoo.co.uk>
2015-07-11 09:40:48 +0100

    complete working example

--- src/SignalRMessenger/SignalRMessenger.Web/Global.asax.cs
+++ src/SignalRMessenger/SignalRMessenger.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
 
 namespace SignalRMessenger.Web
 {
@@ -16,6 +17,9 @@ namespace SignalRMessenger.Web
     {
         protected void Application_Start()
         {
+            // Register the default hubs route: ~/signalr/hubs
+            RouteTable.Routes.MapHubs(new HubConfiguration() { EnableCrossDomain = true });
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
---
+++ src/SignalRMessenger/SignalRMessenger.Web/SignalRHubs/MessengerHub.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using SignalRMessenger.Web.Models;
+
+namespace SignalRMessenger.Web.SignalRHubs
+{
+    public class MessengerHub : Hub
+    {
+        private static readonly List<ChatClient> ConnectedClientsList = new List<ChatClient>();
+
+        public void Join(ChatClient chatClient)
+        {
+            ConnectedClientsList.Add(chatClient);
+            Groups.Add(Context.ConnectionId, "groupa");
+        }
+
+        public void PushMessegeToClients(string msg)
+        {
+            Clients.All.addMessage(msg);
+        }
+    }
+}
